f1eac05a61d8d372ff12ab6e20c47c1553f9e726
Andy Michaels <anstmichaels@gmail.com>
2025-09-29 16:50:53 -0700

    Updated petsc, slepc versions. Added permetis, scotch as petsc dependencies

---
+++ confdefs.h
@@ -0,0 +1,19 @@
+/* confdefs.h */
+#define PACKAGE_NAME "emopt"
+#define PACKAGE_TARNAME "emopt"
+#define PACKAGE_VERSION "version-0.5"
+#define PACKAGE_STRING "emopt version-0.5"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define STDC_HEADERS 1
+#define HAVE_MPI_H 1
+#define HAVE_BOOST_GEOMETRY_GEOMETRIES_POLYGON_HPP 1
