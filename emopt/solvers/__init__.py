ebe397598055d8ffaaa55821208f4b11bd912662
acil <rizki.arif@kaist.ac.kr>
2026-03-27 18:10:14 +0900

    Make GPU solver import optional when CUDA lib is unavailable

--- emopt/solvers/__init__.py
+++ emopt/solvers/__init__.py
@@ -3,7 +3,10 @@ from .fdtd import *
 from .modes import *
 from .simulation import *
 
-from . import fdtd_gpu
+try:
+    from . import fdtd_gpu
+except Exception:
+    fdtd_gpu = None
 
 __all__ = ['Maxwell2DTE', 'Maxwell2DTM', 'Maxwell3D', 'MaxwellSolver', 'Mode1DTE',  'Mode1DTM',
            'Mode2D']
